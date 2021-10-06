3380140268d9252ad5d1b89ac8ba3028b4affc4c
Christoper <c.blake@hotmail.se>
2021-10-06 10:32:05 +0200

    Add project files.

---
+++ LMSGroupOne/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LMSGroupOne.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
