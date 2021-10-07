ce624ee8da500d54248a9c0fa76d2cd5a42e0e03
Christoper <c.blake@hotmail.se>
2021-10-07 13:45:36 +0200

    LMS.Core Empty entities

---
+++ LMS.Core/Models/Entities/Person.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.Core.Models.Entities
+{
+    public class Person : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int CourseId { get; set; }
+
+        //NavProperties
+        public ICollection<Document> Documents { get; set; }
+        public Course Course { get; set; }
+
+    }
+}
