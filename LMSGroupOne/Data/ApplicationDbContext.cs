dd963969d851e20713611ec658720376d552abac
pplnews <rn_mk@hotmail.com>
2021-10-07 16:32:57 +0200

    Migration

--- LMSGroupOne/Data/ApplicationDbContext.cs
+++ LMSGroupOne/Data/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using LMS.Core.Models.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -8,9 +10,19 @@ namespace LMSGroupOne.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        DbSet<Activity> Activities { get; set; }
+        DbSet<ActivityType> ActivityTypes { get; set; }
+        DbSet<Course> Courses { get; set; }
+        DbSet<Document> Documents { get; set; }
+        DbSet<Module> Modules { get; set; }
+        DbSet<Person> Persons { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
--- LMSGroupOne/Data/Migrations/20211007141707_AddEntities.Designer.cs
+++ LMSGroupOne/Data/Migrations/20211007141707_AddEntities.Designer.cs
@@ -4,14 +4,16 @@ using LMSGroupOne.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace LMSGroupOne.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211007141707_AddEntities")]
+    partial class AddEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
