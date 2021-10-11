13529d0549587fa166a7e0fc91f1ecc5a94dc32c
pplnews <rn_mk@hotmail.com>
2021-10-11 15:36:48 +0200

    MapperProfile

---
+++ LMS.Data/Data/MapperProfile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using LMS.Core.Models.Entities;
+using LMS.Core.Models.ViewModels;
+using LMS.Core.Models.ViewModels.Course;
+
+namespace LMS.Data.Data
+{
+    public class MapperProfile:Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<Course, CreateCourseViewModel>().ReverseMap();
+        }
+    }
+}
