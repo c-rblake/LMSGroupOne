38ffdc7e8f90eea8bf485483cf2a91ab6d900a0d
jennicka@home.se <jennicka@home.se>
2021-10-11 15:21:32 +0200

    Identity scaffolded

--- LMSGroupOne/Startup.cs
+++ LMSGroupOne/Startup.cs
@@ -33,14 +33,14 @@ namespace LMSGroupOne
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
-            services.AddDefaultIdentity<Person>
-                (options =>
+            services.AddDefaultIdentity<Person>(options =>
                 {
                     options.SignIn.RequireConfirmedAccount = false;
                     options.Password.RequireDigit = false;
                     options.Password.RequiredLength = 3;
                     options.Password.RequireNonAlphanumeric = false;
                     options.Password.RequireUppercase = false;
+                    options.Password.RequireLowercase = false;
                 })
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
