a4a624128076b50177d944cafcf6322e46d3dff3
jennicka@home.se <jennicka@home.se>
2021-10-18 10:26:01 +0200

    Added methods on HomeController, created viewmodel and view

---
+++ LMS.Core/Models/ViewModels/Document/AddDocumentViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LMS.Core.Models.ViewModels.Document
+{
+    public class AddDocumentViewModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Please enter document name")]
+        [MaxLength(40, ErrorMessage = "Max length for document name is 40 characters")]
+        [Display(Name = "Document name")]
+        public string Name { get; set; }
+        [MaxLength(100, ErrorMessage = "Max length for document description is 250 characters")]
+        [Display(Name = "Document description")]
+        public string Description { get; set; }
+        public string DocumentUrl { get; set; }
+        public DateTime TimeStamp { get; set; }
+        [Required]
+        public string PersonId { get; set; }
+        public int? ModuleId { get; set; }
+        public int? ActivityId { get; set; }
+        public int? CourseId { get; set; }
+    }
+}
--- LMSGroupOne/Controllers/HomeController.cs
+++ LMSGroupOne/Controllers/HomeController.cs
@@ -5,10 +5,14 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using LMS.Core.Models.ViewModels.Document;
 
 namespace LMSGroupOne.Controllers
 {
@@ -17,12 +21,14 @@ namespace LMSGroupOne.Controllers
         private readonly ILogger<HomeController> _logger;
         private readonly UserManager<Person> _userManager;
         private readonly SignInManager<Person> _signInManager;
+        private readonly IWebHostEnvironment _environment;
 
-        public HomeController(ILogger<HomeController> logger, UserManager<Person> userManager, SignInManager<Person> signInManager)
+        public HomeController(ILogger<HomeController> logger, UserManager<Person> userManager, SignInManager<Person> signInManager, IWebHostEnvironment environment)
         {
             _logger = logger;
             _userManager = userManager;
             _signInManager = signInManager;
+            _environment = environment;
         }
 
         [Authorize]
@@ -37,6 +43,60 @@ namespace LMSGroupOne.Controllers
             return (View());
 
         }
+
+        [Authorize]
+        public IActionResult UploadDocuments()
+        {
+            var addDocumentViewModel = new AddDocumentViewModel { };
+
+            return View(addDocumentViewModel);
+        }
+
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult UploadDocuments(List<IFormFile> postedDocuments, AddDocumentViewModel viewModel)
+        {
+            string wwwPath = this._environment.WebRootPath;
+
+            string documentsDirectoryPath = Path.Combine(wwwPath, "documents");
+
+            if (!Directory.Exists(documentsDirectoryPath))
+            {
+                Directory.CreateDirectory(documentsDirectoryPath);
+            }
+
+            List<string> uploadedDocuments = new List<string>();
+
+            foreach (IFormFile postedDocument in postedDocuments)
+            {
+                string fileName = Path.GetFileName(postedDocument.FileName);
+
+                string documentUrl = Path.Combine(documentsDirectoryPath, fileName);
+
+                var personId = _userManager.GetUserId(User);
+
+                using (FileStream stream = new FileStream(documentUrl, FileMode.Create))
+                {
+                    postedDocument.CopyTo(stream);
+                    uploadedDocuments.Add(fileName);
+                    var document = new Document
+                    {
+                        Name = fileName,
+                        DocumentUrl = documentUrl,
+                        TimeStamp = DateTime.Now,
+                        PersonId = personId
+                    };
+
+                    //db.Documents.Add(document);
+                    //await db.SaveChangesAsync();
+
+                }
+            }
+            return RedirectToAction(nameof(Index), "Home");
+
+        }
+
 
         public IActionResult Privacy()
         {
